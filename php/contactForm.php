95104d881699e5deb81a0b6d690d2cbb57b169bb
bennyk53 <bennyk.flames@gmail.com>
2020-07-12 13:20:36 -0400

    Update contactForm.php

--- php/contactForm.php
+++ php/contactForm.php
@@ -1,15 +1,17 @@
 <?php
     $name = $_POST['name'];
     $visitorEmail = $_POST['email'];
-    $subject = $_POST['subject']
+    $subject = $_POST['subject'];
     $message = $_POST['message'];
 
     $email_body = "From: $name \n"."Email: $visitorEmail\n"."Message:\n\n $message.";
     
-    $to = "bennyk.flames@gmail.com"
+    $to = "bennyk.flames@gmail.com";
 
     $headers = "From: $visitorEmail \r\n";
 
-    mail($to, $subject, $email_body, $headers)
+    mail($to, $subject, $email_body, $headers);
+
+    header("Location: contactUs.html");
 
 ?>
