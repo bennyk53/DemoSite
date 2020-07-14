630f7756406d6ce2b76b4240b6440c6101c690a2
bennyk53 <bennyk.flames@gmail.com>
2020-07-14 11:13:22 -0400

    ReadyToGo

--- js/index.js
+++ js/index.js
@@ -8,3 +8,48 @@ function plain(image) {
 	var newImage = "img/smokeDomeTemplate.png";
 	image.src = newImage;
 }
+
+function img1on(image) {
+	var newImage = "img/players.jpg";
+	image.src = newImage;
+			
+}
+
+function img1off(image) {
+	var newImage = "img/IMG_2650.JPG";
+	image.src = newImage;
+}
+
+function img2on(image) {
+	var newImage = "img/about.jpg";
+	image.src = newImage;
+			
+}
+
+function img2off(image) {
+	var newImage = "img/IMG_4432.jpg";
+	image.src = newImage;
+}
+
+function img3on(image) {
+	var newImage = "img/news.jpg";
+	image.src = newImage;
+			
+}
+
+function img3off(image) {
+	var newImage = "img/Snapchat-1574946677.jpg";
+	image.src = newImage;
+}
+
+function img4on(image) {
+	var newImage = "img/contact.jpg";
+	image.src = newImage;
+			
+}
+
+function img4off(image) {
+	var newImage = "img/Snapchat-2096356519.jpg";
+	image.src = newImage;
+}
+
